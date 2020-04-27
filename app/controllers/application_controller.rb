741d3825b2b89749efceb3e60d687b74172966af
Bruno <brunosilvapalmas@gmail.com>
2020-04-27 10:56:38 -0300

    front-end da aplicação

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: -> { request.env['PATH_INFO'] == '/'}
   protect_from_forgery unless: -> { request.format.json? }
+
+  #devise espera o metodo, para ser redirecionado
+  def after_sign_in_path_for(resource)
+    '/discovery'
+  end
 end
+  
